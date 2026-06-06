37a4e6f967894fc9ee1ef7397b7c47b928e8f90a
aren-adamian-ucla <aadamian03@ucla.edu>
2026-06-06 16:14:08 -0700

    Final_test_tuningpart1

--- ros2_ws/src/robot/robot/fsm_helpers/task_planner.py
+++ ros2_ws/src/robot/robot/fsm_helpers/task_planner.py
@@ -5,7 +5,8 @@ User-defined mission tasks. Set MODE below
 from robot.fsm_helpers import course_parameters as cp
 from robot.fsm_helpers.course_parameters import SQ
 
-MODE = "helpers"
+MODE = "FINAL"
+# MODE = "helpers"
 # MODE = "RANDOM"
 
 if MODE == "RANDOM":
@@ -200,17 +201,17 @@ elif MODE == "FINAL":
     tasks = [
         {"state": "WAIT", "trigger": "green_light"},
         {"state": "NAV", "path_planner": "pp", "vision": "burger_bun", "waypoints": cp.WP_BURGER_BUN_1},
-        # {"state": "MANIP", "cmd": "pick"},
-        # {"state": "NAV", "path_planner": "pp", "vision": "burger_patty", "waypoints": cp.WP_BURGER_PATTY},
-        # {"state": "MANIP", "cmd": "pick"},
-        # {"state": "NAV", "path_planner": "pp", "vision": "burger_bun", "waypoints": cp.WP_BURGER_BUN_2},
-        # {"state": "MANIP", "cmd": "pick"},
-        # {"state": "NAV", "path_planner": "pp", "waypoints": [cp.WP1, cp.WP2, cp.WP3, cp.WP4]},
-        # {"state": "NAV", "path_planner": "lapf", "vision": "face", "waypoints": cp.WP_CUSTOMER_ID},
-        # {"state": "IDENT"},
-        # {"state": "PLAN"},
-        # {"state": "NAV", "path_planner": "pp", "vision": "customer", "waypoints": cp.WP_CUSTOMER_1},
-        # {"state": "MANIP", "cmd": "place"},
-        # {"state": "NAV", "path_planner": "pp", "vision": "stop_sign", "waypoints": cp.WP_STOP_SIGN},
-        # {"state": "NAV", "path_planner": "pp", "waypoints": cp.WP5}
+        {"state": "MANIP", "cmd": "pick"},
+        {"state": "NAV", "path_planner": "pp", "vision": "burger_patty", "waypoints": cp.WP_BURGER_PATTY},
+        {"state": "MANIP", "cmd": "pick"},
+        {"state": "NAV", "path_planner": "pp", "vision": "burger_bun", "waypoints": cp.WP_BURGER_BUN_2},
+        {"state": "MANIP", "cmd": "pick"},
+        {"state": "NAV", "path_planner": "pp", "waypoints": [cp.WP1, cp.WP2, cp.WP3, cp.WP4]},
+        {"state": "NAV", "path_planner": "lapf", "vision": "face", "waypoints": cp.WP_CUSTOMER_ID},
+        {"state": "IDENT"},
+        {"state": "PLAN"},
+        {"state": "NAV", "path_planner": "pp", "vision": "customer", "waypoints": cp.WP_CUSTOMER_1},
+        {"state": "MANIP", "cmd": "place"},
+        {"state": "NAV", "path_planner": "pp", "vision": "stop_sign", "waypoints": cp.WP_STOP_SIGN},
+        {"state": "NAV", "path_planner": "pp", "waypoints": cp.WP5}
     ]
